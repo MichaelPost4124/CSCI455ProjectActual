14c45a02a651088d362a79e91e0b10b1fbf88826
Wyatt Hanson <wyatt.j.hanson@ndus.edu>
2024-04-30 20:03:32 -0500

    Save before Making the login page the first one to show up.

--- CSCI455ProjectActual/LoginPage.Designer.cs
+++ CSCI455ProjectActual/LoginPage.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LoginPage";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            Login_UND = new PictureBox();
+            Login_Button = new Button();
+            ((System.ComponentModel.ISupportInitialize)Login_UND).BeginInit();
+            SuspendLayout();
+            // 
+            // Login_UND
+            // 
+            Login_UND.Image = (Image)resources.GetObject("Login_UND.Image");
+            Login_UND.Location = new Point(1, -2);
+            Login_UND.Name = "Login_UND";
+            Login_UND.Size = new Size(801, 101);
+            Login_UND.SizeMode = PictureBoxSizeMode.StretchImage;
+            Login_UND.TabIndex = 0;
+            Login_UND.TabStop = false;
+            // 
+            // Login_Button
+            // 
+            Login_Button.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Login_Button.Location = new Point(349, 338);
+            Login_Button.Name = "Login_Button";
+            Login_Button.Size = new Size(114, 44);
+            Login_Button.TabIndex = 1;
+            Login_Button.Text = "Login";
+            Login_Button.UseVisualStyleBackColor = true;
+            Login_Button.Click += Login_Button_Click;
+            // 
+            // LoginPage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Login_Button);
+            Controls.Add(Login_UND);
+            Name = "LoginPage";
+            Text = "LoginPage";
+            ((System.ComponentModel.ISupportInitialize)Login_UND).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox Login_UND;
+        private Button Login_Button;
     }
 }
--- CSCI455ProjectActual/LoginPage.cs
+++ CSCI455ProjectActual/LoginPage.cs
@@ -16,5 +16,10 @@ namespace CSCI455ProjectActual
         {
             InitializeComponent();
         }
+
+        private void Login_Button_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

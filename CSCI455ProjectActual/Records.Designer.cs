6724cf6b31c83d485a2b39566a0cb00ad163a2c5
WyattHanson <wyatt.j.hanson@und.edu>
2024-05-01 20:12:10 -0500

    Button Functionality to Navigate to all Pages and All pages Navigation

--- CSCI455ProjectActual/Records.Designer.cs
+++ CSCI455ProjectActual/Records.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Records";
+            Paitent_Records_Button = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // Paitent_Records_Button
+            // 
+            Paitent_Records_Button.BackColor = Color.Green;
+            Paitent_Records_Button.ForeColor = SystemColors.ButtonHighlight;
+            Paitent_Records_Button.Location = new Point(348, 369);
+            Paitent_Records_Button.Name = "Paitent_Records_Button";
+            Paitent_Records_Button.Size = new Size(104, 23);
+            Paitent_Records_Button.TabIndex = 11;
+            Paitent_Records_Button.Text = "Records";
+            Paitent_Records_Button.UseVisualStyleBackColor = false;
+            Paitent_Records_Button.Click += Paitent_Records_Button_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Green;
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Location = new Point(631, 403);
+            button5.Name = "button5";
+            button5.Size = new Size(81, 23);
+            button5.TabIndex = 10;
+            button5.Text = "Settings";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Green;
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(86, 403);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 23);
+            button4.TabIndex = 9;
+            button4.Text = "Patient Info";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Green;
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(348, 403);
+            button3.Name = "button3";
+            button3.Size = new Size(104, 23);
+            button3.TabIndex = 8;
+            button3.Text = "Prescriptions";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Records
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Paitent_Records_Button);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Name = "Records";
+            Text = "Records";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button Paitent_Records_Button;
+        private Button button5;
+        private Button button4;
+        private Button button3;
     }
 }
--- CSCI455ProjectActual/Settings.cs
+++ CSCI455ProjectActual/Settings.cs
@@ -16,5 +16,42 @@ namespace CSCI455ProjectActual
         {
             InitializeComponent();
         }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //patient data button click
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PatientDataPage patientDataPage = new PatientDataPage();
+            this.Close();
+            patientDataPage.Show();
+        }
+
+        //Records page button click
+        private void Paitent_Records_Button_Click(object sender, EventArgs e)
+        {
+            Records records = new Records();
+            this.Close();
+            records.Show();
+        }
+
+        //Prescriptions page button click
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Prescriptions prescriptions = new Prescriptions();
+            this.Close();
+            prescriptions.Show();
+        }
+
+        //Settings button click
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Settings settings = new Settings();
+            this.Close();
+            settings.Show();
+        }
     }
 }
